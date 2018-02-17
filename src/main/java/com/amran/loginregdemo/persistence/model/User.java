ca653c3b76e07450a44ed59a952aed989029986d
Administrator <admin@example.com>
2018-02-17 21:56:05 +0600

    Amran-Home

--- src/main/java/com/amran/loginregdemo/persistence/model/User.java
+++ src/main/java/com/amran/loginregdemo/persistence/model/User.java
@@ -1,7 +1,8 @@
 package com.amran.loginregdemo.persistence.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 /**
  * @Project:LoginRegDemoExample
@@ -11,4 +12,104 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_com_user")
 public class User {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private byte[] image;
+    private String email;
+    private String mobile;
+    private String gender;
+    private Date dob;
+    private String password;
+
+    public User() {
+        super();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "username", unique = true, nullable = false)
+    @NotNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Lob
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    @Column(length = 60)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/amran/loginregdemo/persistence/repository/UserRepository.java
+++ src/main/java/com/amran/loginregdemo/persistence/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.amran.loginregdemo.persistence.repository;
 
+import com.amran.loginregdemo.persistence.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -8,5 +10,14 @@ import org.springframework.stereotype.Repository;
  * @Date:11/02/2018
  */
 @Repository
-public interface UserRepository extends Jpa{
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findById(Long id);
+
+    User findByEmail(String email);
+
+    User findByUsername(String username);
+
+    @Override
+    void delete(User user);
 }
