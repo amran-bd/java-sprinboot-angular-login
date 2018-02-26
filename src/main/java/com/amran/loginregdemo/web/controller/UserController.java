4cea25844d37f2d5946066d9b6c49d653759e42f
Administrator <admin@example.com>
2018-02-27 01:43:04 +0600

    API

--- src/main/java/com/amran/loginregdemo/web/controller/UserController.java
+++ src/main/java/com/amran/loginregdemo/web/controller/UserController.java
@@ -1,10 +1,21 @@
 package com.amran.loginregdemo.web.controller;
 
+import com.amran.loginregdemo.web.dto.UserRegisterDto;
+import com.amran.loginregdemo.web.error.ApiError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.io.Serializable;
 import java.util.Locale;
 
@@ -13,11 +24,16 @@ import java.util.Locale;
  * @author:Md.Amran-Hossain
  * @Date:11/02/2018
  */
-@Controller
+@RestController
+@RequestMapping(path = "/user",produces = MediaType.APPLICATION_JSON_VALUE )
 public class UserController implements Serializable{
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping(value="/",method = RequestMethod.GET)
-    public String homepage(){
-        return "index";
+    @RequestMapping(value = "/createUser",  method = RequestMethod.POST)
+    public ResponseEntity<Object> createUser(@RequestBody @Valid UserRegisterDto userRegisterDto){
+        LOGGER.debug("User  create: {}", userRegisterDto);
+
+        final ApiError apiError = new ApiError(HttpStatus.EXPECTATION_FAILED, "User creation failed.", "error occurred");
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 }
